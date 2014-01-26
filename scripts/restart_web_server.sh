25abc53a970b646c5417e938c9f4dfede47d80fb
jcrossler <jay.crossler@gmail.com>
2014-01-26 00:03:48 -0500

    Updated Berksfile to remove calling ohai - it was erroring trying to call nginx, which called it.

--- scripts/restart_web_server.sh
+++ scripts/restart_web_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 sudo service nginx restart
-sudo /var/lib/geoq/bin/uwsgi --ini /var/lib/geoevgeoqvents.ini &
+sudo killall -9 uwsgi
+sudo /var/lib/geoq/bin/uwsgi --ini /var/lib/geoq/geoq.ini --py-auto-reload=3 &
 echo "Nginx and uWSGI should have restarted"
